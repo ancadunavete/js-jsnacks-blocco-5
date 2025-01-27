cfc04e134bbfaa4e4225e9fdba1e662a9d7e534c
ancadunavete <anca.dnvt@gmail.com>
2025-01-27 17:35:01 +0100

    add: 8th ex

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const marco = students.find(student => student.class==="3C");
+console.log(marco.class)
